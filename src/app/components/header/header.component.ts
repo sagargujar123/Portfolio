5fdc95b3b087ebf97b2b1b3b8ee529a6e91916c4
sagargujar123 <sagarg21jar157@gmail.com>
2025-07-15 15:57:56 +0530

    header code added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [
+    ButtonModule,
+    MenubarModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+items = [
+    { label: 'About', command: () => this.scrollTo('about') },
+    { label: 'Skills', command: () => this.scrollTo('skills') },
+    { label: 'Projects', command: () => this.scrollTo('projects') },
+    { label: 'Contact', command: () => this.scrollTo('contact') },
+  ];
 
+  scrollTo(id: string) {
+    document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
+  }
+  
 }
