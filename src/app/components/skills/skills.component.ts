fd965d12c1e78593b832105e7c9960604e8d0d9e
sagargujar123 <sagarg21jar157@gmail.com>
2025-07-15 16:02:47 +0530

    skills code added

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  standalone: true,
+  imports: [
+    TagModule
+  ],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills = ['Angular', '.NET Core', 'Entity Framework', 'SQL Server', 'SCSS', 'PrimeNG', 'JWT Auth'];
 }
